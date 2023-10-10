ecc28d98c5fd73b6ee09bc4db377998da1b6e8f8
090TYPE <117640194+090TYPE@users.noreply.github.com>
2023-10-10 19:19:43 +0300

    3 задание c#

---
+++ C#/3 зад/3 зад/Program.cs
@@ -0,0 +1,26 @@
+﻿public sealed class Circle
+{   
+    private double radius;    
+    public Circle(double radius)    
+    {      
+        this.radius = radius;     
+    }
+    public double Calculate(Func<double, double> op)
+        
+    {   
+        return op(radius);    
+    }   
+}    
+internal class Program  
+{     
+    static void Main()    
+    {     
+        double radius = 3.25;     
+        Circle circle = new Circle(radius);  
+        Func<double, double> calculateCircumference = r => 3.14 * (2 * r);  
+        double circumference = circle.Calculate(calculateCircumference); 
+        Console.WriteLine($"Длина окружности с радиусом {radius} равна {circumference}");
+    }
+}
+
+
